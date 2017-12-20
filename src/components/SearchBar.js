20229a88bf185d916c765dc2077d0268dbd57d6e
Tianhang Liu <tianhanl@usc.edu>
2017-12-20 17:16:46 +0800

    add style to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const SearchBar = ({ value, onChange }) => (
-  <div className="search-bar">
-    <input
-      type="text"
-      value={value}
-      onChange={e => {
-        e.preventDefault();
-        onChange(e.target.value);
-      }}
-    />
-  </div>
+  <input
+    className="search-bar"
+    type="text"
+    placeholder="Search your target items"
+    value={value}
+    onChange={e => {
+      e.preventDefault();
+      onChange(e.target.value);
+    }}
+  />
 );
 
 export default SearchBar;
